e27e9244c8a97078d4b0e7d5b1cc27229d7d0079
Jimmy Kittiyachavalit <jkit@fb.com>
2016-10-25 16:53:11 -0700

    Show deployment id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
 class HomeController < ApplicationController
+
+  def info
+    file = File.join(Rails.root, 'config', 'deployment_id')
+    if File.exists?(file)
+      deployment_id = File.read(file).chomp
+      message = "Deployment id is #{deployment_id}"
+    else
+      message = "Couldn't determine id"
+    end
+    render text: message
+  end
+
 end
